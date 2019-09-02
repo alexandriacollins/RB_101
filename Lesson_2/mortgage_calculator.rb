c5a8d9e25718a6d9548321f734b1b298ea0716df
Alexandria Collins <alexandria.k.collins@gmail.com>
2019-09-02 16:04:35 -0500

    Updated mortgage calc.

--- Lesson_2/mortgage_calculator.rb
+++ Lesson_2/mortgage_calculator.rb
@@ -4,16 +4,54 @@ end
 
 prompt "Welcome to the Mortgage/Car Loan Calculator App!"
 
-prompt "What is the loan amount(USD)?"
-loan = gets.chomp.to_i
+loop do
+  prompt "What is the loan amount(USD)?"
+  loan = ''
+  loop do
+    loan = gets.chomp
+    if loan.empty? || loan.to_f <= 0
+      prompt "Number must be greater than 0. Try again."
+    else
+      break
+    end
+  end
 
-prompt "What is the APR(%)?"
-apr = gets.chomp.to_f / 100 # converts to percentage
-month_interest = apr / 12
+  prompt "What is the APR(%)?"
+  apr = ''
+  loop do
+    apr = gets.chomp
+    if apr.empty? || apr.to_f <= 0
+      prompt "Percentage must be greater than 0. Try again."
+    else
+      break
+    end
+  end
 
-prompt "What is the loan duration(years)?"
-duration = gets.chomp.to_i * 12 # converts to months
 
-m = loan * (month_interest / (1 - (1 + month_interest)**(-duration)))
+  prompt "What is the loan duration(years)?"
+  duration = ''
+  loop do
+    duration = gets.chomp
+    if duration.empty? || duration.to_i <= 0
+      prompt "Duration must be greater than 0. Try again."
+    else
+      break
+    end
+  end
 
-prompt "Your monthly payment would be: $#{m.round(2)}"
+  annual_interest = apr.to_f / 100
+  monthly_interest = annual_interest / 12
+  duration_months = duration.to_i * 12
+
+  monthly_payment = loan.to_f * (monthly_interest / 
+                           (1 - (1 + monthly_interest)**(-duration_months)))
+  prompt "Your monthly payment is: $#{format('%.2f', monthly_payment)}"
+
+  prompt "Do you want do another calculation?"
+  answer = gets.chomp.downcase
+
+  break unless answer.include?('y')
+end
+
+prompt "Thank you for using this application!"
+prompt "Have a nice day!"
