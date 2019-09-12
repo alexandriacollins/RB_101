3a8247c7dfc577fd9ccd81ef397b11f77b2665cd
Alexandria Collins <alexandria.k.collins@gmail.com>
2019-09-12 11:05:58 -0500

    Twenty One V2

--- Lesson_6/twenty_one.rb
+++ Lesson_6/twenty_one.rb
@@ -1,6 +1,10 @@
+require 'pry'
+
 # Constants
 SUITS = ['H', 'D', 'S', 'C']
 VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+GAME_GOAL = 21
+DEALER_MAX_STAY = 17
 
 # Methods
 def prompt(msg)
@@ -26,7 +30,7 @@ def totals(cards)
   end
 
   values.select { |value| value == "A" }.count.times do
-    sum -= 10 if sum > 21
+    sum -= 10 if sum > GAME_GOAL
   end
 
   sum
@@ -36,17 +40,14 @@ def hit(deck, user_hand)
   user_hand << (deck.delete(deck.sample))
 end
 
-def busted?(hand)
-  totals(hand) > 21
+def busted?(total)
+  total > GAME_GOAL
 end
 
-def detect_result(player_cards, dealer_cards)
-  player_total = totals(player_cards)
-  dealer_total = totals(dealer_cards)
-
-  if player_total > 21
+def detect_result(player_total, dealer_total)
+  if player_total > GAME_GOAL
     :player_busted
-  elsif dealer_total > 21
+  elsif dealer_total > GAME_GOAL
     :dealer_busted
   elsif dealer_total < player_total
     :player
@@ -57,8 +58,8 @@ def detect_result(player_cards, dealer_cards)
   end
 end
 
-def display_result(player_cards, dealer_cards)
-  result = detect_result(player_cards, dealer_cards)
+def display_result(player_total, dealer_total)
+  result = detect_result(player_total, dealer_total)
 
   case result
   when :player_busted
@@ -81,6 +82,22 @@ def play_again?
   answer.downcase.start_with?('y')
 end
 
+def grand_output(player_hand, dealer_hand)
+  puts "=========="
+  prompt "Dealer has #{dealer_hand}, for a total of: #{totals(dealer_hand)}"
+  prompt "Player has #{player_hand}, for a total of: #{totals(player_hand)}"
+  puts "=========="
+end
+
+def display_total_wins(player_wins, dealer_wins)
+  prompt "You: #{player_wins} wins"
+  prompt "Dealer: #{dealer_wins} wins"
+end
+
+# Wins tracker
+player_wins = 0
+dealer_wins = 0
+
 # Code for game
 loop do # Start of new game
   prompt "Welcome to Twenty One"
@@ -94,8 +111,11 @@ loop do # Start of new game
     dealer_hand << deck.pop
   end
 
+  player_total = totals(player_hand)
+  dealer_total = totals(dealer_hand)
+
   prompt "Dealer has #{dealer_hand[0]} and ?"
-  prompt "You have: #{player_hand[0]} and #{player_hand[1]}, for a total of #{totals(player_hand)}."
+  prompt "You have: #{player_hand[0]} and #{player_hand[1]}, for a total of #{player_total}."
 
   # Player turn
   loop do
@@ -109,49 +129,69 @@ loop do # Start of new game
 
     if player_answer == 'h'
       player_hand << deck.pop
+      player_total = totals(player_hand)
       prompt "You chose to hit!"
       prompt "Your cards are now: #{player_hand}"
-      prompt "Your total is now: #{totals(player_hand)}"
+      prompt "Your total is now: #{player_total}"
     end
 
-    break if player_answer == 's' || busted?(player_hand)
+    break if player_answer == 's' || busted?(player_total)
   end
 
-  if busted?(player_hand)
-    display_result(player_hand, dealer_hand)
+  if busted?(player_total)
+    grand_output(player_hand, dealer_hand)
+    display_result(player_total, dealer_total)
+    dealer_wins += 1
+    display_total_wins(player_wins, dealer_wins)
     play_again? ? next : break
   else
-    prompt "You stayed at #{totals(player_hand)}."
+    prompt "You stayed at #{player_total}."
   end
 
   # Dealer turn
   prompt "Dealer turn..."
 
   loop do
-    break if totals(dealer_hand) >= 17
+    break if dealer_total >= DEALER_MAX_STAY
 
     prompt "Dealer hits!"
     dealer_hand << deck.pop
+    dealer_total = totals(dealer_hand)
     prompt "Dealer's cards are now #{dealer_hand}"
   end
 
-  if busted?(dealer_hand)
-    prompt "Dealer total is now: #{totals(dealer_hand)}"
-    display_result(player_hand, dealer_hand)
+  if busted?(dealer_total)
+    prompt "Dealer total is now: #{dealer_total}"
+    grand_output(player_hand, dealer_hand)
+    display_result(player_total, dealer_total)
+    player_wins += 1
+    display_total_wins(player_wins, dealer_wins)
     play_again? ? next : break
   else
-    prompt "Dealer stays at #{totals(dealer_hand)}"
+    prompt "Dealer stays at #{dealer_total}"
   end
 
   # dealer and player stays
-  puts "=========="
-  prompt "Dealer has #{dealer_hand}, for a total of: #{totals(dealer_hand)}"
-  prompt "Player has #{player_hand}, for a total of: #{totals(player_hand)}"
-  puts "=========="
+  grand_output(player_hand, dealer_hand)
+  display_result(player_total, dealer_total)
 
-  display_result(player_hand, dealer_hand)
+  # Update win total
+  if dealer_total < player_total
+    player_wins += 1
+  else dealer_total > player_total
+    dealer_wins += 1
+  end
+  display_total_wins(player_wins, dealer_wins)
 
-  break unless play_again?
+  if player_wins == 5
+    prompt "You are the grand champion!"
+    break
+  elsif dealer_wins == 5
+    prompt "The Dealer is the grand champion!"
+    break
+  end
+
+  break unless play_again? 
 end
 
 prompt "Thanks for playing Twenty One! Good bye!"
